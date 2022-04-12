0f21d3e3a235202f19c53f45de5f75fe4ea9836c
jimmisimon98 <103520372+jimmisimon98@users.noreply.github.com>
2022-04-12 11:52:06 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,13 @@
-sudo snap install amass
-sudo apt install amass
-sudo apt install httpie
+sudo apt install git
 sudo apt install golang-go
 sudo apt install cargo
 sudo apt install python3-pip
 sudo apt install curl
 sudo apt install jq
-pip3 install -r ./requirements.txt
-git clone https://github.com/Edu4rdSHL/findomain.git tools/findomain
-cd tools/findomain/
-cargo build --release
-sudo cp target/release/findomain /bin/
-cd ../../
-GO111MODULE=on go get -u -v github.com/lc/gau
-go get -u github.com/tomnomnom/httprobe
-go get -u github.com/tomnomnom/assetfinder
-go get -u github.com/tomnomnom/anew
-GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
-export PATH=$PATH:$(go env GOPATH)/bin
+cargo install NtHiM
+pip install httpie
+pip3 install -r ./tools/requirements.txt
+go install github.com/hahwul/dalfox/v2@latest
+chmod +x tools/*
+export GOPATH=$HOME/go
+mkdir domain
