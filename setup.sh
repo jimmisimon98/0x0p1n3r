ffb9122ce87294a344f77f738cb62c26342d658d
jimmisimon98 <103520372+jimmisimon98@users.noreply.github.com>
2022-04-12 12:03:32 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,17 @@ sudo apt install golang-go
 sudo apt install python3-pip
 sudo apt install curl
 sudo apt install jq
-pip install httpie
+pip3 install httpie
 pip3 install -r ./tools/requirements.txt
+go get -u github.com/lc/gau
+go get -u github.com/tomnomnom/httprobe
+go get -u github.com/tomnomnom/assetfinder
+go get -u github.com/tomnomnom/waybackurls
+go get -v github.com/tomnomnom/anew@latest
+go get -u github.com/tomnomnom/qsreplace
 go install github.com/hahwul/dalfox/v2@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 chmod +x tools/*
 export GOPATH=$HOME/go
 mkdir domain
